4aa5a5ef3e1f734787c57737f3281546f557175c
duraisamy-git <duraisamy22122000@gmail.com>
2023-11-28 17:28:34 +0530

    first commit

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import AddPost from "../../components/addPost/AddPost";
+import Feeds from "../../components/feeds/Feeds";
+import UserProfile from "../../components/userprofile/UserProfile";
+
+
+export default function Profile() {
+  return (
+    <>
+    <UserProfile />
+    <AddPost />
+    <Feeds />
+    </>
+  )
+}
